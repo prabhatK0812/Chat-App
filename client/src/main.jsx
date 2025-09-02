ac074197692b0e7817117eacdade1575b1969fa4
prabhatK0812 <prabhat07248311@gmail.com>
2025-09-02 23:06:36 +0530

    praba

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,8 +7,8 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 // for adding context support
-import { AuthProvider } from '../context/AuthContext.jsx'
-import { ChatProvider } from '../context/ChatContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
+import { ChatProvider } from './context/ChatContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
